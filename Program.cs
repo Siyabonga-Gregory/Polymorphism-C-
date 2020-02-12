2828edaea3fc134ab6ee591a2a9d8f3c858f977b
Siyabonga Gregory <huge.fuze@gmail.com>
2020-02-12 17:10:14 +0200

    Polymorphism
    Please, check the youtube tutorial to see how i did this code: https://www.youtube.com/watch?v=9kBzpktrwHU

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    public class Toyota
+    {
+        public virtual void Details(){
+            Console.WriteLine("Toyota");
+        }
+    }
+
+    public class Yaris : Toyota
+    {
+        public override void Details() {
+            Console.WriteLine("Yaris");
+        }
+    }
+
+    public class Avanza : Toyota
+    {
+        public override void Details(){
+            Console.WriteLine("Avanza");
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Toyota[] toyotaCars = new Toyota[3];
+            toyotaCars[0] = new Toyota();
+            toyotaCars[1] = new Yaris();
+            toyotaCars[2] = new Avanza();
+
+            foreach (Toyota car in toyotaCars){
+                car.Details();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+Please, check the youtube tutorial to see how i did this code: https://www.youtube.com/watch?v=9kBzpktrwHU
